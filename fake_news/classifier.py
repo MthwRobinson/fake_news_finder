scaling word2vec instead of L1 normalization, fix issues when calculating fscore and precision and recall are 0. Workaround pickle bug # issue24658

fake_news/classifier.py
@@ -8,7 +8,9 @@ else:
     import cPickle as pickle
 import random
 import numpy as np
+import pandas as pd
 from scipy.stats import ttest_ind
+import os
 
 
 class NewsClassifier():
@@ -42,8 +44,26 @@ class NewsClassifier():
         # Only import the model if the vectorizer has changed
         if self.vec != vec:
             vec_file = self.dir + '/models/vecs/' + vec + '.pickle'
-            with open(vec_file, 'rb') as f:
-                self.df = pickle.load(f)
+            if os.path.isfile(vec_file):
+                with open(vec_file, 'rb') as f:
+                    self.df = pickle.load(f)
+            else:
+                #Below is the workaround for bug in MacOS, Python 3.4 described here:
+                ##http://deo.im/2016/09/20/Pickle-can-t-dump-2GB-file/
+                vec_file = self.dir + '/models/vecs/' + vec + '.0.pickle'
+                if os.path.isfile(vec_file):
+                    max_index = 0
+                    vec_file = self.dir + '/models/vecs/' + vec + '.' + str(max_index+1) + '.pickle'
+                    while os.path.isfile(vec_file):
+                        max_index = max_index + 1
+                        vec_file = self.dir + '/models/vecs/' + vec + '.' + str(max_index+1) + '.pickle'
+                    list_of_dataframes = []
+                    for i in range(0,max_index+1):
+                        vec_file = self.dir + '/models/vecs/' + vec + '.' + str(i) + '.pickle'
+                        with open(vec_file, 'rb') as f:
+                            list_of_dataframes.append(pickle.load(f))
+                    self.df = pd.concat(list_of_dataframes)
+
         self.vec = vec
 
     def train_test_sets(self, pct, random_seed = 21189):
@@ -109,20 +129,24 @@ class NewsClassifier():
                 tp += 1
             if y[i] == 0 and ypred[i] == 0:
                 tn += 1
-            if y[i] == 1 and ypred[i] == 0:
-                fp += 1
             if y[i] == 0 and ypred[i] == 1:
+                fp += 1
+            if y[i] == 1 and ypred[i] == 0:
                 fn += 1
 
         # Compute summary statistics
+
         total = tp+tn+fp+fn
+        print("Total:" + str(total) + "| True Positive:" +str(tp) + "| True Negative:"  + str(tn) + "| False Positive:"+str(fp) + "| False Negative:" + str(fn))
         accuracy = (tp+tn) / total
-        precision = tp / (tp+fp)
-        recall = 0
+        precision = None 
+        if tp + fp != 0:
+            precision = tp / (tp+fp)
+        recall = None
         if tp + fn != 0:
             recall = tp / (tp+fn)
-        fscore = 0
-        if precision+recall != 0 :
+        fscore = None
+        if precision is not None and recall is not None and precision + recall != 0:
             fscore = 2*((precision*recall)/(precision+recall))
         stats = {
             'accuracy' : accuracy,

fake_news/word2vec.py
@@ -33,12 +33,15 @@ class Word2VecSimple:
       indeces = indeces + 1
       self.normalized_model.append(self.model[word])
 
-    #self.normalized_model = np.array(self.normalized_model)
-    #self.normalized_model = np.apply_along_axis(scaling, axis=0, arr=self.normalized_model)
-    w = csr_matrix(np.asarray(self.normalized_model))
-    self.normalized_model = normalize(w, norm='l1', axis=0)
-    self.normalized_model = self.normalized_model.todense().tolist()
-    #print(self.normalized_model[:,26:])
+    # Scaling:
+    self.normalized_model = np.array(self.normalized_model)
+    self.normalized_model = np.apply_along_axis(scaling, axis=0, arr=self.normalized_model)
+    self.normalized_model = self.normalized_model.tolist()
+    # Normalizing:
+    #   w = csr_matrix(np.asarray(self.normalized_model))
+    #   self.normalized_model = normalize(w, norm='l1', axis=0)
+    #   self.normalized_model = self.normalized_model.todense().tolist()
+    #   print(self.normalized_model[:,26:])
     print("Finish model")
 
 
@@ -63,10 +66,17 @@ class Word2VecSimple:
       #  if word.lower() in self.vocabs:
       #    words_factors_list_of_lists.append(self.model[self.vocabs[word.lower()]])
 
-      for word in self.model.wv.vocab:
-        if word.lower() in doc_dictionary:
-          index = self.word_index_dict[word]
-          words_factors_list_of_lists.append(self.normalized_model[index])
+      for word in doc_dictionary:
+        if word.lower() in self.vocabs:
+          original_case_word = self.vocabs[word.lower()]
+          index_in_normalized_model = self.word_index_dict[original_case_word]
+          model_to_append = self.normalized_model[index_in_normalized_model]
+          words_factors_list_of_lists.append(model_to_append)
+
+      #for word in self.model.wv.vocab:
+      #  if word.lower() in doc_dictionary:
+      #    index = self.word_index_dict[word]
+      #    words_factors_list_of_lists.append(self.normalized_model[index])
 
       vectorization_item = self.vectorize_document(words_factors_list_of_lists)
       if vectorization_item is not None: